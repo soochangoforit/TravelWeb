c1c33f7354542087462a509831eae2e2579a566c
soochangoforit <tncksdl05@gmail.com>
2022-05-08 04:03:10 +0900

    feat (UserRepository) : Security Service에서 자동 로그인을 해주는 loadUserByUsername() 을 구현하기 위해서 findByLoginId Method 추가

--- src/main/java/com/cloud/web/repository/UserRepository.java
+++ src/main/java/com/cloud/web/repository/UserRepository.java
@@ -2,8 +2,16 @@ package com.cloud.web.repository;
 
 import com.cloud.web.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+
+    @Query(value = "select u from User u where u.loginId = :loginId")
+    Optional<User> findByLoginId(@Param("loginId") String loginId);
+
 }
