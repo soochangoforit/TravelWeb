3e5868404cf2fb511d066ff79ff11219b0f726b2
soochangoforit <tncksdl05@gmail.com>
2022-05-13 20:36:08 +0900

    feat (Login Failure) : 로그인 실패시 , 서버로부터 날라오는 Error를 변수로 받아서 각 Exception에 따라서 다른 메시지를 js-alret 창에 표시

---
+++ src/main/java/com/cloud/web/config/auth/LoginFailureHandler.java
@@ -0,0 +1,43 @@
+package com.cloud.web.config.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLEncoder;
+
+@Configuration
+public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException, ServletException {
+        String errorMessage;
+        if (exception instanceof BadCredentialsException) {
+            errorMessage = "아이디 또는 비밀번호가 맞지 않습니다. 다시 확인해 주세요.";
+        } else if (exception instanceof InternalAuthenticationServiceException) {
+            errorMessage = "내부적으로 발생한 시스템 문제로 인해 요청을 처리할 수 없습니다. 관리자에게 문의하세요.";
+        } else if (exception instanceof UsernameNotFoundException) {
+            errorMessage = "계정이 존재하지 않습니다. 회원가입 진행 후 로그인 해주세요.";
+        } else if (exception instanceof AuthenticationCredentialsNotFoundException) {
+            errorMessage = "인증 요청이 거부되었습니다. 관리자에게 문의하세요.";
+        } else {
+            errorMessage = "알 수 없는 이유로 로그인에 실패하였습니다 관리자에게 문의하세요.";
+        }
+        errorMessage = URLEncoder.encode(errorMessage, "UTF-8");
+
+        setDefaultFailureUrl("/loginForm?alertmsg="+errorMessage); // 로그인 실패시 /loginForm 경로로 쿼리스트링을 보내는데 출력하고자 하는 error 문자열을 한글로 인코딩헤서 함께 보낸다.
+        super.onAuthenticationFailure(request, response, exception);
+    }
+
+}
+
