404591a94bb4c7fd5762ecf08cd0c5372c928abc
soochangoforit <tncksdl05@gmail.com>
2022-05-22 19:09:15 +0900

    feat (foodBoard) : Rest API를 활용하기 위해서 HiddenHttpMethodFiler를 추가하였다.
    다양한 put , delete method를 사용하기 위해서 bean으로 등록했다.

--- src/main/java/com/cloud/web/WebApplication.java
+++ src/main/java/com/cloud/web/WebApplication.java
@@ -2,7 +2,9 @@ package com.cloud.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -10,6 +12,13 @@ public class WebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
+	}
+
+
+	//@DeleteMapping, @PutMapping 등의 최신 매핑기능을 이용하기 위해선 아래와 같은 코드를 작성해야 한다고 한다.
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
+		return new HiddenHttpMethodFilter();
 	}
 
 }
